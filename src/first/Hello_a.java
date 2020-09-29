bc2832cebd7d0e8a78007abce8bb5eedfd927ead
Jimmyluluu <luu21@kimo.com>
2020-09-29 09:35:49 +0800

    first commit

---
+++ src/first/Hello_a.java
@@ -0,0 +1,13 @@
+package first;
+
+public class Hello_a {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I'm 魯敬元");
+		System.out.println("My nid is D0885995");
+		System.out.println("This is lab 1 hw");
+		
+	}
+
+}
