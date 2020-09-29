81e0e88a619015d72681ba0892b35b10c3c19fb2
Jimmyluluu <luu21@kimo.com>
2020-09-29 09:43:23 +0800

    second

--- src/first/Hello_a.java
+++ src/first/Hello_a.java
@@ -4,7 +4,7 @@ public class Hello_a {
 
 	public static void main(String[] args) {
 		
-		System.out.println("I'm 魯敬元");
+		System.out.println("I'm LU");
 		System.out.println("My nid is D0885995");
 		System.out.println("This is lab 1 hw");
 		
